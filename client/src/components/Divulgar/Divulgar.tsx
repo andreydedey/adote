bdc951e0730fa65d0eb054a3061a693bbf6a6b65
Andrey Oliveira <dedey.cardoso@gmail.com>
2025-02-14 19:02:44 -0300

    publicize page complet

--- client/src/components/Divulgar/Divulgar.tsx
+++ client/src/components/Divulgar/Divulgar.tsx
@@ -1,34 +1,37 @@
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function Divulgar() {
   return (
     <form action="#" className="p-6 bg-gray-100 rounded-lg">
       <h1 className="text-center text-xl font-bold mb-4">Quero Divulgar</h1>
       <div className="flex gap-4 mb-2">
+        <label htmlFor="file_input" className="mb-2 text-sm font-medium">
+          Fotos do Pet
+        </label>
         <input
           type="file"
           aria-describedby="file_input_help"
           id="file_input"
           className="flex-grow text-sm border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:bg-gray-300s focus:outline-none"
         />
-        <label htmlFor="file_input" className="mb-2 text-sm font-medium">
-          Fotos do Pet
-        </label>
       </div>
       <label htmlFor="name" className="block mb-2 text-sm font-medium">
         Nome
       </label>
       <input
         type="text"
-        className="block bg-gray-50 border border-gray-300 text-sm rounded-lg focus:border-blue-500 w-full p-2.5 mb-2"
+        className="block bg-gray-50 border border-gray-300 text-sm rounded-lg focus:outline-blue-500 w-full p-2.5 mb-2"
       />
 
-      <div className="flex gap-4">
+      <div className="flex gap-4 mb-2">
         <div className="flex-grow">
           <label htmlFor="name" className="block mb-2 text-sm font-medium">
             Estado
           </label>
           <input
             type="text"
-            className="block bg-gray-50 border border-gray-300 text-sm rounded-lg focus:border-blue-500 w-full p-2.5"
+            className="block bg-gray-50 border border-gray-300 text-sm rounded-lg focus:outline-blue-500 w-full p-2.5"
           />
         </div>
         <div className="flex-grow">
@@ -37,43 +40,58 @@ export function Divulgar() {
           </label>
           <input
             type="text"
-            className="block bg-gray-50 border border-gray-300 text-sm rounded-lg focus:border-blue-500 w-full p-2.5"
+            className="block bg-gray-50 border border-gray-300 text-sm rounded-lg focus:outline-blue-500 w-full p-2.5"
           />
         </div>
       </div>
-      <div>
-        <label
-          htmlFor="phone-input"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Phone number:
-        </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 19 18"
-            >
-              <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z" />
-            </svg>
-          </div>
-          <input
-            type="text"
-            id="phone-input"
-            aria-describedby="helper-text-explanation"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            placeholder="(00) 1234-7890"
-            required
+      <label
+        htmlFor="phone-input"
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >
+        Phone number:
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
+          <FontAwesomeIcon
+            className="w-4 h-4 text-gray-500 icon="
+            icon={faPhone}
           />
         </div>
-        <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500">
-          Selecione um número de Telefone que siga o formato
-        </p>
+        <input
+          type="text"
+          id="phone-input"
+          aria-describedby="helper-text-explanation"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-500 block w-full ps-10 p-2.5"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          placeholder="(00) 1234-7890"
+          required
+        />
       </div>
+      <p id="helper-text-explanation" className="my-2 text-sm text-gray-500">
+        Selecione um número de Telefone que siga o formato
+      </p>
+      <label
+        htmlFor="countries"
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >
+        Raça
+      </label>
+      <select
+        id="countries"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-blue-500 block w-full p-2.5"
+      >
+        <option value="US">Vira-lata</option>
+        <option value="CA">Golden Retriever</option>
+        <option value="FR">Border Collie</option>
+        <option value="DE">Poodle</option>
+      </select>
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-3"
+      >
+        Salvar
+      </button>
     </form>
   );
 }
